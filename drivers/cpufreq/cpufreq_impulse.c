4fd47cf35abdf2e9334a11515160228d905ffe81
Ryan Andri <ryanandri@linuxmail.org>
2017-06-11 05:44:31 +0700

    cpufreq: impulse: take a neat some codes

--- drivers/cpufreq/cpufreq_impulse.c
+++ drivers/cpufreq/cpufreq_impulse.c
@@ -224,12 +224,11 @@ static unsigned int freq_to_above_hispeed_delay(unsigned int freq)
 	spin_lock_irqsave(&above_hispeed_delay_lock, flags);
 
 	for (i = 0; i < nabove_hispeed_delay - 1 &&
-			freq >= above_hispeed_delay[i+1]; i += 2)
-		;
-
-	ret = above_hispeed_delay[i];
+			freq >= above_hispeed_delay[i+1]; i += 2);
+		ret = above_hispeed_delay[i];
 
 	spin_unlock_irqrestore(&above_hispeed_delay_lock, flags);
+
 	return ret;
 }
 
@@ -241,11 +240,12 @@ static unsigned int freq_to_targetload(unsigned int freq)
 
 	spin_lock_irqsave(&target_loads_lock, flags);
 
-	for (i = 0; i < ntarget_loads - 1 && freq >= target_loads[i+1]; i += 2)
-		;
+	for (i = 0; i < ntarget_loads - 1 &&
+			freq >= target_loads[i+1]; i += 2);
+		ret = target_loads[i];
 
-	ret = target_loads[i];
 	spin_unlock_irqrestore(&target_loads_lock, flags);
+
 	return ret;
 }
 
