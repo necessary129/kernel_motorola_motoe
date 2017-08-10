3327c80e375b8eedd27f89bc8ae7354ebe956446
Ryan Andri <ryanandri@linuxmail.org>
2017-08-11 02:46:59 +0700

    Add Template zip for MM/N, move MM and N into same tree
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ARGS=$1
+case $ARGS in
+     'M' | 'm')
+		DK=M;;
+     *)
+		DK=N;;
+esac
+
+rm -f /home/ryanandri/android/android_kernel_motoe/clarity-$DK-condor/boot/zImage-dtb
+rm -f /home/ryanandri/android/android_kernel_motoe/clarity-$DK-condor/system/lib/modules/*.ko
+rm -f /home/ryanandri/android/android_kernel_motoe/arch/arm/boot/zImage
+
+mv /home/ryanandri/android/android_kernel_motoe/arch/arm/boot/zImage-dtb /home/ryanandri/android/android_kernel_motoe/clarity-$DK-condor/boot
+
+# get modules into one place
+find -name "*.ko" -exec cp {} /home/ryanandri/android/android_kernel_motoe/clarity-$DK-condor/system/lib/modules \;
+sleep 2
+
+cd /home/ryanandri/android/android_kernel_motoe/clarity-$DK-condor
+
+DATE=`date +%d-%m-%Y`;
+zip -r /home/ryanandri/android/release/Clarity-$DK-$DATE.zip *
+
