826eaf63bb0c9f1c408f7f07a1f44b9207da0706
Ryan Andri <ryanandri@linuxmail.org>
2017-08-16 16:21:16 +0700

    cpufreq: import pegasusq/impulse/nightmare governors
    * untouch from dorimanx @ https://github.com/dorimanx/Dorimanx-LG-G2-D802-Kernel
    
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -383,6 +383,15 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lionheart)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
+extern struct cpufreq_governor cpufreq_gov_pegasusq;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
+extern struct cpufreq_governor cpufreq_gov_nightmare;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nightmare)
 #endif
 
 
