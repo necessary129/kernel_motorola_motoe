a135f83c18f5403fe6b3094d3b7e1e576237179b
Ryan Andri <ryanandri@linuxmail.org>
2017-08-30 00:44:54 +0700

    clean.sh: fix derp
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- clean.sh
+++ clean.sh
@@ -2,8 +2,8 @@
 
 rm -f /home/ryanandri/android/android_kernel_motoe/arch/arm/mach-msm/smd_rpc_sym.c
 make clean && make mrproper
-rm -f /home/ryanandri/android/android_kernel_motoe/clarity-M-condor/boot/zImage-dtb
+rm -f /home/ryanandri/android/android_kernel_motoe/clarity-N-condor/boot/zImage-dtb
 rm -f /home/ryanandri/android/android_kernel_motoe/clarity-M-condor/boot/zImage-dtb
 rm -f /home/ryanandri/android/android_kernel_motoe/clarity-N-condor/system/lib/modules/*.ko
-rm -f /home/ryanandri/android/android_kernel_motoe/clarity-N-condor/system/lib/modules/*.ko
+rm -f /home/ryanandri/android/android_kernel_motoe/clarity-M-condor/system/lib/modules/*.ko
 
