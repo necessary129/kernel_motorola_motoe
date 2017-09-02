dd78ff19d6adc3525a0dd7fdae9d1a96f85ef81e
Ryan Andri <ryanandri@linuxmail.org>
2017-09-02 19:24:58 +0700

    block: io switcher: use powersuspend instead of FB notifier
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- block/iosched_switcher.c
+++ block/iosched_switcher.c
@@ -16,7 +16,7 @@
 #include <linux/blkdev.h>
 #include <linux/blk_types.h>
 #include <linux/elevator.h>
-#include <linux/fb.h>
+#include <linux/powersuspend.h>
 
 #define NOOP_IOSCHED "noop"
 #define RESTORE_DELAY_MS (10000)
@@ -59,45 +59,36 @@ static void change_all_elevators(struct list_head *head, bool use_noop)
 		change_elevator(r, use_noop);
 }
 
-static int fb_notifier_callback(struct notifier_block *nb,
-		unsigned long action, void *data)
-{
-	struct fb_event *evdata = data;
-	int *blank = evdata->data;
-
-	if (action != FB_EVENT_BLANK)
-		return NOTIFY_OK;
-
-	switch (*blank) {
-	case FB_BLANK_UNBLANK:
-		/*
-		 * Switch back from noop to the original iosched after a delay
-		 * when the screen is turned on.
-		 */
-		schedule_delayed_work(&restore_prev,
-				msecs_to_jiffies(RESTORE_DELAY_MS));
-		break;
-	default:
-		/*
-		 * Switch to noop when the screen turns off. Purposely block
-		 * the fb notifier chain call in case weird things can happen
-		 * when switching elevators while the screen is off.
-		 */
-		cancel_delayed_work_sync(&restore_prev);
-		change_all_elevators(&req_queues.list, true);
-	}
-
-	return NOTIFY_OK;
-}
-
-static struct notifier_block fb_notifier_callback_nb = {
-	.notifier_call = fb_notifier_callback,
-};
-
 static void restore_prev_fn(struct work_struct *work)
 {
 	change_all_elevators(&req_queues.list, false);
 }
+
+static void is_power_suspend(struct power_suspend *h)
+{
+	/*
+	 * Switch to noop when the screen turns off. Purposely block
+	 * the fb notifier chain call in case weird things can happen
+	 * when switching elevators while the screen is off.
+	 */
+	cancel_delayed_work_sync(&restore_prev);
+	change_all_elevators(&req_queues.list, true);
+}
+
+static void is_power_resume(struct power_suspend *h)
+{
+	/*
+	 * Switch back from noop to the original iosched after a delay
+	 * when the screen is turned on.
+	 */
+	schedule_delayed_work(&restore_prev,
+			msecs_to_jiffies(RESTORE_DELAY_MS));
+}
+
+static struct power_suspend is_power_suspend_handler = {
+	.suspend = is_power_suspend,
+	.resume = is_power_resume,
+};
 
 int init_iosched_switcher(struct request_queue *q)
 {
@@ -119,7 +110,7 @@ int init_iosched_switcher(struct request_queue *q)
 static int iosched_switcher_core_init(void)
 {
 	INIT_DELAYED_WORK(&restore_prev, restore_prev_fn);
-	fb_register_client(&fb_notifier_callback_nb);
+	register_power_suspend(&is_power_suspend_handler);
 
 	return 0;
 }
